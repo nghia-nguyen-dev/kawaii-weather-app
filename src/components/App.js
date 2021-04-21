c27cc4268b76030c43f3e62b61006de5f93d233b
Nghia <hello.nghia22@gmail.com>
2021-04-21 12:56:54 -0400

    style: Add border thickness variables

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,3 @@ const App = () => {
 };
 
 export default App;
-
-
-`https://api.openweathermap.org/data/2.5/onecall?lat=42.6508288&lon=-71.303168&exclude=minutely,hourly,alerts&appid=9b8dab94ee12342b026cfe5b50cd1f1c`
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom";
 import App from "components/App";
 import "css/index.css";
 
-navigator.geolocation.getCurrentPosition(position => {
-	console.log(position.coords.latitude, position.coords.longitude);
-});
+// navigator.geolocation.getCurrentPosition(position => {
+// 	console.log(position.coords.latitude, position.coords.longitude);
+// });
 
 ReactDOM.render(<App />, document.querySelector("#root"));
