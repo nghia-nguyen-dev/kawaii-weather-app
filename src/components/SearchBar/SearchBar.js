5aad58254be88a734f57011264ce72d44ddcafc5
Nghia <hello.nghia22@gmail.com>
2021-04-24 19:08:14 -0400

    feat: Implement fetchCoordinates

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 import SearchIcon from "components/SearchBar/SearchIcon";
 import SearchInput from "components/SearchBar/SearchInput";
-import Form  from "components/SearchBar/parts/Form"
+import Form from "components/SearchBar/parts/Form";
+import axios from "axios";
+
+const fetchCoordinates = location => {
+	const base = `http://api.openweathermap.org/geo/1.0/direct?`;
+
+	return axios
+		.get(base, {
+			params: {
+				q: location,
+				limit: 1,
+				appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+			},
+		})
+		.then(({ data }) => {
+			return {
+				lat: data[0].lat,
+				lon: data[0].lon,
+			};
+		});
+};
 
 const SearchBar = () => {
 	const [location, setLocation] = useState("");
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		console.log(location);
+		fetchCoordinates(location).then(res => console.log(res));
 	};
 
 	return (
