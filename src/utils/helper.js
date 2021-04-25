69393bd5750ca52b9530808c89fa112233c27edc
Nghia <hello.nghia22@gmail.com>
2021-04-25 18:55:21 -0400

    feat: Add wind speed multiplier

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,2 +1,10 @@
 export const toCelsius = f => ((f - 32) * 5) / 9;
 export const renderedTemp = (isCelsius, temp) => Math.round(isCelsius ? toCelsius(temp) : temp);
+
+/* To determine meter width in css (wind speed in mph)
+** max width of bar is 100%
+** max wind speed 75
+** need to use a multiplier
+*/
+const maxWindSpeed = 75;
+export const windSpeedMultiplier = 100 / maxWindSpeed;
