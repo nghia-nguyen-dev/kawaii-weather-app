1fc7a951e57416dd24492048e1576bf987dd0923
Nghia <hello.nghia22@gmail.com>
2021-04-25 20:33:40 -0400

    feat: Add hover logic to weather metrics to display diff data

--- src/components/WeatherMetrics/CloudMeter.js
+++ src/components/WeatherMetrics/CloudMeter.js
@@ -1,11 +1,25 @@
 import Container from "components/WeatherMetrics/parts/Container";
 import Fill from "components/WeatherMetrics/parts/Fill";
 import SubHead from "components/WeatherMetrics/parts/SubHead";
+import { useState } from "react";
 
 const CloudMeter = ({ weatherData }) => {
+	const [onHover, setOnHover] = useState(false);
+
+	const handleMouseEnterLeave = e => {
+		setOnHover(prevState => !prevState);
+	};
+
+	const renderedSubhead = onHover
+		? `${Math.round(weatherData.current?.clouds)} %`
+		: "cloudy";
+
 	return (
-		<Container>
-			<SubHead>cloudy</SubHead>
+		<Container
+			onMouseEnter={handleMouseEnterLeave}
+			onMouseOut={handleMouseEnterLeave}
+		>
+			<SubHead>{renderedSubhead}</SubHead>
 			<Fill width={`${weatherData.current?.clouds}%`} />
 		</Container>
 	);
