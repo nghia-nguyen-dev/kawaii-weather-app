245b79f8ae5a5ee76e2b1d13a976abdf7ebcf92e
Nghia <hello.nghia22@gmail.com>
2021-05-18 22:15:51 -0400

    style: Update padding and spacing in hero

--- src/components/Hero/parts/Flex.js
+++ src/components/Hero/parts/Flex.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 const Flex = styled.div`
 	display: flex;
 	justify-content: flex-end;
-	margin-top: 102px;
+	margin-top: 72px;
 	gap: 30px;
 	@media (max-width: 744px) {
-		margin-top: 72px;
+		/* margin-top: 72px; */
 	}
 	@media (min-width: 1600px) {
 		justify-content: space-between;
--- src/components/Hero/parts/Svg.js
+++ src/components/Hero/parts/Svg.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Svg = styled.svg`
 	width: 140px;
 	height: min-content;
+	margin-top: 28px;
 `;
 
 export default Svg;
